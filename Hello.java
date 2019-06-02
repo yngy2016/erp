e4cd1ebb5175aba84eb4d6f86e8904479e6cfd9f
gaoyan <gaoyan2269904451@qq.com>
2019-06-02 14:11:28 +0800

    Test Merge

--- Hello.java
+++ Hello.java
@@ -12,6 +12,5 @@ public class Test
 		//System.out.println("Hello World !!!!");
 		System.out.println("Hello World !!!!");
 		System.out.println("Hello World !!!!");
-		System.out.println("Hello");
 	}
 }
