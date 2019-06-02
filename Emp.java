93ced3f73cf0f9fe28b5317cfdf58f9ac3fa9da1
gaoyan <gaoyan2269904451@qq.com>
2019-06-02 17:28:13 +0800

    Change Emp.java In dev branch

--- Emp.java
+++ Emp.java
@@ -5,6 +5,7 @@ public class Emp implements java.io.Serializable
 	private Integer EmpNo;
 	private String ename;
 	private String username;
+	private String password;
 
 	public Emp(Integer EmpNo,String ename){
 		this.EmpNo = EmpNo;
