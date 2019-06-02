7cac4b99191f827ea349d6eeb7f9adebe750fbbe
gaoyan <gaoyan2269904451@qq.com>
2019-06-02 13:26:22 +0800

    Change Hello.java Filr In master

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ package cn.gaoyan.test ;
 public class Test
 {
 	public static void main(String args[]){
+		Admin admin = new Admin();
+		System.out.println("管理员操作"+admin);
 		System.out.println("Hello Git !!!!");
 		//System.out.println("Hello Git !!!!");
 		//System.out.println("Hello World !!!!");
